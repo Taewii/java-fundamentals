6435df314307c6246211afe6f46fa90738b02192
Taewii <p3rfec7@abv.bg>
2018-08-11 21:10:42 +0300

    Added the structure of the exam

---
+++ Java OOP Advanced/10. Unit Testing - Panzer Unit Tests/PanzerTests/src/test/java/panzer/models/miscellaneous/VehicleAssemblerTest.java
@@ -0,0 +1,63 @@
+package panzer.models.miscellaneous;
+
+import org.junit.Before;
+import org.junit.Test;
+import panzer.contracts.*;
+
+public class VehicleAssemblerTest {
+
+    private Assembler vehicleAssembler;
+
+    @Before
+    public void setUp() throws Exception {
+        this.vehicleAssembler = new VehicleAssembler();
+    }
+
+    @Test
+    public void getTotalWeight() {
+
+    }
+
+    @Test
+    public void getTotalPrice() {
+
+    }
+
+    @Test
+    public void getTotalPriceWithoutParts() {
+
+    }
+
+    @Test
+    public void getTotalAttackModification() {
+
+    }
+
+    @Test
+    public void getTotalDefenseModification() {
+
+    }
+
+    @Test
+    public void getTotalHitPointModification() {
+
+    }
+
+    @Test
+    public void testAddArsenalPartCollectionSize() {
+
+    }
+
+    @Test
+    public void testAddArsenalPart() {
+
+    }
+
+    @Test
+    public void addShellPart() {
+    }
+
+    @Test
+    public void addEndurancePart() {
+    }
+}
